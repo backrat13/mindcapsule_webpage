98a2b01d5d79b15a8477a24d664e76e8163bba88
frank_the_rat <frank_the_rat@pop-os.lan>
2025-03-16 06:14:07 -0400

    Initial gh-pages commit

---
+++ first_article.md
@@ -0,0 +1,34 @@
+Title: Welcome to Mind Capsule
+
+Date: 2024-03-16 03:08:00
+
+Category: Articles
+
+Tags: introduction, welcome
+
+
+# Welcome to Project Mind Capsule
+
+
+    Mind Capsule is an AI-powered personal companion designed to enhance daily life>
+
+
+2. Company Description:
+
+
+    Mind Capsule is dedicated to developing and deploying AI companion technology t>
+
+    Our initial product focuses on a "Companion Version" that establishes a foundat>
+
+    We are committed to creating an intuitive and accessible user experience, ensur>
+
+    We will focus on a subscription based business model, where users pay a monthly>
+
+3.  Products and Services:
+
+
+    Mind Capsule Companion Version:
+
+        Voice-activated AI companion that engages in natural conversations.
+
+        Personalized memory prompts and reminders. 
---
+++ pelicanconf.py
@@ -0,0 +1,34 @@
+AUTHOR = 'frank hymer'
+SITENAME = 'mindcapsule'
+SITEURL = ""
+
+PATH = "content"
+
+TIMEZONE = 'EST'
+
+DEFAULT_LANG = 'en'
+THEME = 'notmyidea'
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Pelican", "https://getpelican.com/"),
+    ("Python.org", "https://www.python.org/"),
+    ("Jinja2", "https://palletsprojects.com/p/jinja/"),
+    ("Kickstarter", "https://www.kickstarter.com/projects/mindcapsule2025/1410003339/"),
+)
+# Social widget
+SOCIAL = (
+    ("You can add links in your config file", "#"),
+    ("Another social link", "#"),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
